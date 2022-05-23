6885d97e4f71375afc2df1d191a0b6a8bc2bb16e
Deborah Atwiine <deborahatwiine75@gmail.com>
2022-05-23 04:56:15 +0300

    fail test

--- test/test.js
+++ test/test.js
@@ -1,2 +1,11 @@
 const assert = require('chai').assert;
 const tdd = require('../multiply');
+
+describe('Test for multiply', () => {
+    
+    it('test 1', () => {
+        //Assert is used in here
+        assertEqual(multiply(1, 1), 1);
+    })
+
+})
